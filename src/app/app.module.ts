fa64b41ef424cfccd9358e570c7cfb2c894e2f11
Ganesh Maher <ganeshmaheer2020@gmail.com>
2025-04-17 23:27:18 +0530

    About page loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactFormComponent } from './Components/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
